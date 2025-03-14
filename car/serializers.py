5caaf6f0d68a28992a90c2b182bbc61383c94d00
Ilia Terentev <wenhun1@gmail.com>
2025-03-14 15:56:02 +0200

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,30 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(required=True, max_length=64)
+    model = serializers.CharField(required=True, max_length=64)
+    horse_powers = serializers.IntegerField(
+        required=True, max_value=1914, min_value=1)
+    is_broken = serializers.BooleanField(required=True)
+    problem_description = serializers.CharField(required=False)
+
+    def create(self, validated_data: dict) -> Car:
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance: Car, validated_data: dict) -> Car:
+        instance_manufacturer = validated_data.get(  # noqa: F841
+            "manufacturer", instance.manufacturer)
+        instance_model = validated_data.get(  # noqa: F841
+            "model", instance.model)
+        instance_horse_powers = validated_data.get(  # noqa: F841
+            "horse_powers", instance.horse_powers)
+        instance_is_broken = validated_data.get(  # noqa: F841
+            "is_broken", instance.is_broken)
+        instance_problem_description = validated_data.get(  # noqa: F841
+            "problem_description", instance.problem_description)
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import io
+
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
 from car.models import Car
+
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    data = CarSerializer(car).data
+    return JSONRenderer().render(data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    return serializer.save()
